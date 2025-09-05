be5d17964878df13ae1e7f289aeb9dbde42c89a4
philosuffers <the8xguy@gmail.com>
2025-09-05 14:28:40 +0530

    Madlibs game

---
+++ 6madlibs/madlibs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () 
+{
+    char adverb[30];
+    char noun[30];
+    char adjective [30];
+
+    printf("Enter adverb ");
+    fgets (adverb, sizeof(adverb) , stdin);
+    adverb[strlen(adverb) - 1] = '\0';
+
+    printf("Enter noun ");
+    fgets (noun, sizeof(noun) , stdin);
+    noun[strlen(noun) - 1] = '\0';
+
+    printf("Enter adjective ");
+    fgets (adjective, sizeof(adjective) , stdin);
+    adjective[strlen(adjective) - 1] = '\0';
+
+    printf("he said %s " , adverb);
+    printf ("as he jumped into his covertable %s " , noun);
+    printf ("and drove off with his %s wife" , adjective);
+
+
+    return 0;
+}
